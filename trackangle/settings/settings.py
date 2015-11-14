c532224ae2139588ae5bd3f92826aa7774e4e952
evural <ersanvuralzorlu@gmail.com>
2015-11-14 17:56:38 +0200

    Mysql is linked to web

--- trackangle/settings/settings.py
+++ trackangle/settings/settings.py
@@ -13,11 +13,13 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import treebeard
+import pymysql
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 print("BASE: ", BASE_DIR)
 
+pymysql.install_as_MySQLdb()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
@@ -84,8 +86,12 @@ WSGI_APPLICATION = 'wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trackangle',
+	'USER': 'root',
+	'PASSWORD': 'trackangle',
+	'HOST': os.environ.get('DB_PORT_3306_TCP_ADDR', '127.0.0.1'),
+	'PORT': '3306',
     }
 }
 
